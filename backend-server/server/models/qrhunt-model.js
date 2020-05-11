f83d9bb8de375509eb94e5c5c46c0ffe7476ceee
sintax1 <sintax@obscurepacket.org>
2020-05-11 19:49:25 -0400

    removed node_modules

---
+++ backend-server/server/models/qrhunt-model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const HintSchema = new Schema(
+    {
+        easyImg: { data: Buffer },
+        easyText: { type: String },
+        easyTimer: { type: Number, default: 300 }, // minutes
+        mediumImg: { data: Buffer },
+        mediumText: { type: String },
+        mediumTimer: { type: Number, default: 300 }, // minutes
+        hardImg: { data: Buffer },
+        hardText: { type: String },
+        hardTimer: { type: Number, default: 300 } // minutes
+    }
+);
+
+const QRHuntSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        isRandom: { type: Boolean, default: false},
+        hints: [HintSchema],
+        hintOrder: [{ type: Number, required: true }],
+        history: [{
+            player: { type: String, required: true },
+            place: { type: Number, required: true },
+            score: { type: Number, required: true }
+        }]
+    },
+    { timestamps: true },
+)
+
+module.exports = mongoose.model('qrhunts', QRHuntSchema)
