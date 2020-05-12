3f36bfc3496c6aa39f9420a0e6ef94259080a167
sintax1 <sintax@obscurepacket.org>
2020-05-12 18:26:31 -0400

    moved listen start

--- backend-server/nodejs/index.js
+++ backend-server/nodejs/index.js
@@ -4,23 +4,15 @@ const io = require('socket.io')(server);
 const multer = require('multer')
 const bodyParser = require('body-parser')
 const db = require('./db')
+const user = require('./routes/api/user');
+const admin = require('./routes/api/admin');
+
+server.listen(3000, '0.0.0.0', () => {
+  console.log('App running on http://0.0.0.0:3000')
+})
 
 //const app = Express()
 app.use(bodyParser.json())
-
-const Storage = multer.diskStorage({
-  destination(req, file, callback) {
-    callback(null, './images')
-  },
-  filename(req, file, callback) {
-    callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`)
-  },
-})
-
-const upload = multer({ storage: Storage })
-
-const user = require('./routes/api/user');
-const admin = require('./routes/api/admin');
 
 // User Routes
 app.post('/api/signin', user.signin);
@@ -32,6 +24,17 @@ app.get('/', (req, res) => {
   console.log('GET /')
   res.status(200).send('You can post to /api/upload.')
 })
+
+const Storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, './images')
+  },
+  filename(req, file, callback) {
+    callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`)
+  },
+})
+
+const upload = multer({ storage: Storage })
 
 app.post('/api/upload', upload.array('photo', 3), (req, res) => {
     //console.log(JSON.stringify(req));
@@ -52,6 +55,4 @@ io.on('connection', (socket) => {
   console.log('websocket connection');
 });
 
-server.listen(3000, '0.0.0.0', () => {
-  console.log('App running on http://0.0.0.0:3000')
-})
+
