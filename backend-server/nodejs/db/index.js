712edc0258d7463ca074b65112204be915c87904
sintax1 <sintax@obscurepacket.org>
2020-05-12 13:10:36 -0400

    Added mongo creds

--- backend-server/nodejs/db/index.js
+++ backend-server/nodejs/db/index.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose')
 
+const options = {
+    useNewUrlParser: true,
+    user: 'admin',
+    pass: 'password'
+}
+
 mongoose
-    .connect('mongodb://mongodb:27017/qrhunt', { useNewUrlParser: true })
+    .connect('mongodb://mongodb:27017/qrhunt', options)
     .catch(e => {
         console.error('Connection error', e.message)
     })
