1d966ccb4ed89ffadfadd316e00240e78a5d98d5
sintax1 <sintax@obscurepacket.org>
2020-05-13 15:33:40 -0400

    Added photo retrieval

--- backend-server/nodejs/index.js
+++ backend-server/nodejs/index.js
@@ -18,7 +18,7 @@ server.listen(3000, '0.0.0.0', () => {
 app.use(bodyParser.json())
 
 // Shared Routes
-app.get('/api/photos', photos.getPhotoByName)
+app.get('/api/photos', photos.getPhoto)
 
 // User Routes
 app.post('/api/signin', user.signin);
