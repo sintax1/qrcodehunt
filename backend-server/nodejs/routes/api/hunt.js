41765d06b15bdaca6925db11433a5d05181a0f07
sintax1 <sintax@obscurepacket.org>
2020-05-14 11:15:21 -0400

    Added update hunt rest call

--- backend-server/nodejs/routes/api/hunt.js
+++ backend-server/nodejs/routes/api/hunt.js
@@ -131,7 +131,9 @@ exports.updateHunt = async (req, res) => {
     let id = req.params.id;
     let updates = req.body;
 
-    QRHunt.findOneAndUpdate({_id: id}, {name: 'blah'}, { new: true }, (err, doc) => {
+    console.log(updates);
+
+    QRHunt.findOneAndUpdate({_id: id}, {updates}, { new: true }, (err, doc) => {
         console.log('doc: ' + JSON.stringify(doc));
 
         if (err) {
