97e0abe75ae4d4196c847bd1e2828d35d571bc87
sintax1 <sintax@obscurepacket.org>
2020-05-13 20:29:05 -0400

    Added photo retrieval

--- backend-server/nodejs/routes/api/photos.js
+++ backend-server/nodejs/routes/api/photos.js
@@ -7,9 +7,10 @@ const getChunks = async (doc) => {
     return await collectionChunks.find({files_id : docs._id}).sort({n: 1}).toArray();
 };
 
-const getPhotoByName = async (filename) => {
+const getPhotoByName = async (photoIDs) => {
+    let objIDs = photoIDs.map(function(id) { return ObjectId(id); });
     let images = [];
-    let docs = await collection.find({filename: filename}).toArray();
+    let docs = await collection.find({_id: {$in: objIDs }}).toArray();
 
     //console.log('docs: ' + JSON.stringify(docs));
 
@@ -23,8 +24,6 @@ const getPhotoByName = async (filename) => {
             fileData.push(chunks[j].data.toString('base64'));
         }
 
-        console.log('fileData: ' + JSON.stringify(fileData));
-
         images.push({
             id: docs[i]._id,
             photo: 'data:' + docs[i].contentType + ';base64,' + fileData.join('')
@@ -32,63 +31,20 @@ const getPhotoByName = async (filename) => {
     }
 
     return images;
-    
-    
-    /*
-    function(err, docs) {
-        if(err){
-            console.log('err: ' + err);
-            return null;
-        }
-
-        if(!docs || docs.length === 0){
-            console.log('err: No photos found');
-            return null;
-        } else {
-            console.log('Getting Chunks for: ' + JSON.stringify(docs[0]));
-            //Retrieving the chunks from the db
-            return collectionChunks.find({files_id : docs[0]._id})
-            .sort({n: 1}).toArray(function(err, chunks) {
-                if(err) {
-                    console.log('err: ' + err);
-                    return null;
-                }
-                if(!chunks || chunks.length === 0) {
-                    //No data found
-                    console.log('err: No data found');
-                    return null;
-                }
-
-                let fileData = [];
-                for(let i=0; i<chunks.length; i++) {
-                    //This is in Binary JSON or BSON format, which is stored
-                    //in fileData array in base64 endocoded string format
-
-                    fileData.push(chunks[i].data.toString('base64'));
-                }
-
-                //Display the chunks using the data URI format
-                let finalFile = 'data:' + docs[0].contentType + ';base64,' + fileData.join('');
-                console.log('Adding a photo');
-                return finalFile;
-            });
-        }
-    });
-    */
 };
 
 exports.getPhoto = async (req, res) => {
     console.log(Object.keys(req.body));
     //let photoId = req.body.id;
 
-    let photo = await getPhotoByName("1589395305372-qrhunt-photo-filename.jpg");
+    let photos = await getPhotoByName(["1589395305372"]);
 
-    console.log('photo: ' + photo)
+    console.log('photo: ' + photos)
 
-    if (photo) {
+    if (photos) {
         return res.send({
             success: true,
-            photo: photo
+            photos: photos
         });
     } else {
         return res.send({
