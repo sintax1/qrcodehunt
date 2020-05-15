7494a347a04cfabb4fe905ff70b570d92fcca568
sintax1 <sintax@obscurepacket.org>
2020-05-15 13:24:05 -0400

    updated get hint query

--- backend-server/nodejs/routes/api/hunt.js
+++ backend-server/nodejs/routes/api/hunt.js
@@ -5,7 +5,7 @@ const { getPhotoById } = require('./photo')
 exports.getHunt = async (req, res) => {
     let id = req.params.id;
 
-    Hunt.findById(id, (err, doc) => {
+    Hunt.findById(id, async (err, doc) => {
         if (err) {
           console.log(err);
           return res.send({
