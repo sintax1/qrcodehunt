c0377c59283708fff8cf61fbefd7fe570f34f953
sintax1 <sintax@obscurepacket.org>
2020-05-14 20:41:52 -0400

    changed to single photo upload

--- backend-server/nodejs/index.js
+++ backend-server/nodejs/index.js
@@ -68,11 +68,11 @@ const upload = multer({ storage: DBStorage })
 
 app.post('/api/upload', upload.single('photo'), (req, res) => {
     //console.log(JSON.stringify(req));
-    console.log('file', req.files);
+    console.log('file', req.file);
     console.log('body', req.body);
     res.status(200).json({
       message: 'success!',
-      photos: req.files
+      photo: req.file
     });
 });
 
