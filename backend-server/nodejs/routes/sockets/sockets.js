8677516232e9e19f23b22eecd9f5c03398287635
sintax1 <sintax@obscurepacket.org>
2020-05-17 10:57:14 -0400

    Added websocket handler

--- backend-server/nodejs/routes/sockets/sockets.js
+++ backend-server/nodejs/routes/sockets/sockets.js
@@ -7,7 +7,19 @@ module.exports.listen = function(server) {
     console.log('websocket connection');
     socket.emit('connected');
 
+    // Listen for new players that join the hunt
     socket.on('joinHunt', (data) => {
+      let huntID = data.id;
+      let playerName = data.player;
+
+      // Add this socket session to a room. Each Hunt has their own room.
+      socket.join(huntID);
+
+      // Notify everyone in the room that a new player joined
+      io.sockets.in(huntID).emit('newPlayer', {
+        name: playerName
+      });
+
       console.log('joinHunt: ' + JSON.stringify(data));
     });
   });
