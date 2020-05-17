0033c50908f32076446883723cb650929d00908f
sintax1 <sintax@obscurepacket.org>
2020-05-17 11:22:38 -0400

    Added websocket handler

--- backend-server/nodejs/routes/sockets/sockets.js
+++ backend-server/nodejs/routes/sockets/sockets.js
@@ -2,6 +2,7 @@ var socketio = require('socket.io')
 
 module.exports.listen = function(server) {
   let io = socketio.listen(server);
+  let RoomStates = {};
 
   io.on('connection', (socket) => {
     console.log('websocket connection');
@@ -12,8 +13,30 @@ module.exports.listen = function(server) {
       let huntID = data.id;
       let playerName = data.player;
 
-      // Add this socket session to a room. Each Hunt has their own room.
+      // Check if the room already exists
+      let room = io.manager.rooms[huntID];
+      let isControl = false;
+
+      if (room == undefined) {
+        // Room does not exist, this player gets to control when the hunt starts
+        isControl = true;
+
+        // Populate the state data structure
+        RoomStates[huntID] = {
+          status: 'Waiting for ' + playerName + ' to start the Hunt'
+        };
+      }
+
+      // join the room
       socket.join(huntID);
+
+      if (isControl) {
+        socket.emit('update', {
+          isControl: isControl
+        })
+      }
+
+      
 
       // Notify everyone in the room that a new player joined
       io.sockets.in(huntID).emit('newPlayer', {
@@ -22,7 +45,23 @@ module.exports.listen = function(server) {
 
       console.log('joinHunt: ' + JSON.stringify(data));
     });
+
+    // Listen for general messages
+    socket.on('getStatus', () => {
+      // get the room that this player is in
+      let room = io.sockets.manager.roomClients[socket.id];
+      console.log('room: ' + JSON.stringify(room))
+
+      // Send the status of the room
+      socket.emit('update', {
+        status: RoomStates[room.id].status
+      })
+
+    })
+    
   });
+
+
 
   return io;
 }
