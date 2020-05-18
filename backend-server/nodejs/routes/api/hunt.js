3820636b7017d023f26c686caa14a400ee081b15
sintax1 <sintax@obscurepacket.org>
2020-05-18 14:44:36 -0400

    updated player hint logic

--- backend-server/nodejs/routes/api/hunt.js
+++ backend-server/nodejs/routes/api/hunt.js
@@ -29,7 +29,7 @@ exports.getHunt = async (req, res) => {
 
 // GET api/hunts
 exports.getAllHunts = async (req, res) => {
-  Hunt.find({}, '-steps',(err, docs) => {
+  Hunt.find({}, '-steps.$.hints',(err, docs) => {
       if (err) {
         console.log(err);
         return res.send({
