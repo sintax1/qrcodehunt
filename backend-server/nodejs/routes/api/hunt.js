65fd68929c5344719ad073f0fa11f5c24b06cfdc
sintax1 <sintax@obscurepacket.org>
2020-05-18 14:46:46 -0400

    updated remove player

--- backend-server/nodejs/routes/api/hunt.js
+++ backend-server/nodejs/routes/api/hunt.js
@@ -29,7 +29,7 @@ exports.getHunt = async (req, res) => {
 
 // GET api/hunts
 exports.getAllHunts = async (req, res) => {
-  Hunt.find({}, '-timer',(err, docs) => {
+  Hunt.find({}, '-steps',(err, docs) => {
       if (err) {
         console.log(err);
         return res.send({
