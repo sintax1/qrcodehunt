c474e72e764b6f8b2ceed5c4b10e5a3c4ff9130f
sintax1 <sintax@obscurepacket.org>
2020-05-18 13:44:00 -0400

    updated player hint logic

--- backend-server/nodejs/routes/sockets/sockets.js
+++ backend-server/nodejs/routes/sockets/sockets.js
@@ -20,7 +20,7 @@ module.exports.listen = function(server) {
   // format Steps/Hints and randomize, if necessary
   async function processSteps(hunt) {
     hunt.steps.forEach(function(step, sid, steps) {
-      steps[sid].hints.toObject().forEach(function(hint, hid, hints) {
+      steps[sid].hints[0].toObject().forEach(function(hint, hid, hints) {
         console.log('hint: ' + JSON.stringify(hint));
         hints[hid]
       });
